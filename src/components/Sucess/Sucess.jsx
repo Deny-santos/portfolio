9c818be9cb7835c90a72c105b20298af654794f6
deny <denisson.graca3@gmail.com>
2023-06-27 00:26:04 -0300

    progress bar on the sucess mensage

--- src/components/Sucess/Sucess.jsx
+++ src/components/Sucess/Sucess.jsx
@@ -5,7 +5,9 @@ const Sucess = ({text = "", show = false, top = false}) => {
     return (
         <span className={` message__container ${show ? "show" : ""} ${top ? "top": "bottom"}`}>
             {text}
-            <div></div>
+            <div className='progress'>
+                
+            </div>
         </span>
     )
 }
